52fb1b21e191d08d31fcbdec2262a82c7329bba8
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2025-12-20 21:36:02 -0500

    robust?

--- python/__init__.py
+++ python/__init__.py
@@ -1,11 +1,8 @@
 """
 Humdrum Parser Python Bindings
-
-A Python wrapper for the Humdrum parser DLL.
 """
 
 from .humdrum_parser import HumdrumParser, HumdrumParserError, parse_humdrum, ParseError
 
 __version__ = "0.1.0"
 __all__ = ["HumdrumParser", "HumdrumParserError", "parse_humdrum", "ParseError"]
-
